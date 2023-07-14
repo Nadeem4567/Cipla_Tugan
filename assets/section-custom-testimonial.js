1e36ea6e8451c4b2d541a728a6abb1919de53d5d
Nadeem4567 <nadeem@marmeto.com>
2023-07-14 10:32:48 +0530

    modified customizer settings

--- assets/section-custom-testimonial.js
+++ assets/section-custom-testimonial.js
@@ -1,10 +1,11 @@
+//set flickity wrap around false for mobile on
 if(window.innerWidth < 990){
     document.querySelector(".testimonial_main_container .card_container").setAttribute("data-flickity", `{ "prevNextButtons" : true, "pageDots": true, "draggable": true, "cellAlign": "center", "initialIndex": 1, "wrapAround": false, "bgLazyLoad": 1, "autoPlay": 2500, "friction": 0.175 }`)
 }
 
 window.addEventListener("resize", adjustFlickity);
 
-//set flickity wrap around for desktop
+//set flickity wrap around true for desktop on window resize
 function adjustFlickity(){
     if(window.innerWidth < 990){
         document.querySelector(".testimonial_main_container .card_container").setAttribute("data-flickity", `{ "prevNextButtons" : true, "pageDots": true, "draggable": true, "cellAlign": "center", "initialIndex": 1, "wrapAround": false, "bgLazyLoad": 1, "autoPlay": 2500, "friction": 0.175 }`);
